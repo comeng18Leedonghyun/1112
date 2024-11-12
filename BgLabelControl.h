24c8dbd99a0599b2458e4c74656ecbc66269be36
comeng18Leedonghyun <127932231+comeng18Leedonghyun@users.noreply.github.com>
2024-11-12 14:31:51 +0900

    Create BgLabelControl.h

---
+++ BgLabelControl.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "BgLabelControl.g.h"
+#include "BgLabelControl.h"
+
+namespace winrt::App21::implementation
+{
+    struct BgLabelControl : BgLabelControlT<BgLabelControl>
+    {
+        BgLabelControl() { DefaultStyleKey(winrt::box_value(L"BgLabelControlApp.BgLabelControl")); }
+
+        winrt::hstring Label()
+        {
+            return winrt::unbox_value<winrt::hstring>(GetValue(m_labelProperty));
+        }
+
+        void Label(winrt::hstring const& value)
+        {
+            SetValue(m_labelProperty, winrt::box_value(value));
+        }
+
+        static Microsoft::UI::Xaml::DependencyProperty LabelProperty() { return m_labelProperty; }
+
+        static void OnLabelChanged(Microsoft::UI::Xaml::DependencyObject const&, Microsoft::UI::Xaml::DependencyPropertyChangedEventArgs const&);
+
+    private:
+        static Microsoft::UI::Xaml::DependencyProperty m_labelProperty;
+    };
+}
+namespace winrt::App21::factory_implementation
+{
+    struct BgLabelControl : BgLabelControlT<BgLabelControl, implementation::BgLabelControl>
+    {
+    };
+}
